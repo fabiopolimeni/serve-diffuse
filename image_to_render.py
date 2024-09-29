0b204f17550936884f6634063c94b6eb366d9a31
Fabio Polimeni <fabio@ambient.run>
2024-09-29 11:02:52 +0200

    Improve image_to_render script

--- image_to_render.py
+++ image_to_render.py
@@ -1,4 +1,5 @@
 import argparse
+from email import generator
 import os
 import time
 import torch
@@ -7,6 +8,7 @@ from PIL import Image
 from diffusers import FluxControlNetPipeline, FluxControlNetModel
 from diffusers.models import FluxMultiControlNetModel
 from dotenv import load_dotenv
+from utils import ranged_type
 
 if __name__ == "__main__":
     # Load environment variables from .env file
@@ -27,7 +29,12 @@ if __name__ == "__main__":
 
     parser.add_argument("--image", type=str, required=True)
     parser.add_argument("--prompt", type=str, required=True)
+    parser.add_argument("--steps", type=ranged_type(int, 1, 40), default=4)
+    parser.add_argument("--guidance", type=ranged_type(float, 0, 10), default=3.5)
+    parser.add_argument("--is_depth", type=bool, required=False, default=False)
+    parser.add_argument("--depth_weight", type=ranged_type(float, 0, 1), default=0.7)
     parser.add_argument("--outdir", type=str, default="./outputs")
+    parser.add_argument("--seed", type=int, default=42)
 
     args = parser.parse_args()
 
@@ -43,20 +50,26 @@ if __name__ == "__main__":
 
     timestamp = int(time.time())
 
-    # Create the depth map out of the input image
-    depth_pipe = pipeline(
-        task="depth-estimation",
-        model="depth-anything/Depth-Anything-V2-Base-hf",
-        device=device,
-    )
-    image = Image.open(args.image)
-    depth_image = depth_pipe(image)["depth"]
+    # Create or load the depth map
+    if args.is_depth:
+        # Load the provided depth image
+        depth_image = Image.open(args.image)
+    else:
+        # Create the depth map from input image
+        depth_pipe = pipeline(
+            task="depth-estimation",
+            model="depth-anything/Depth-Anything-V2-Base-hf",
+            device=device,
+        )
+        image = Image.open(args.image)
+        depth_image = depth_pipe(image)["depth"]
 
-    depth_output = (
-        os.path.splitext(os.path.basename(args.image))[0] + f"_depth-{timestamp}.png"
-    )
+        depth_output = (
+            os.path.splitext(os.path.basename(args.image))[0]
+            + f"_depth-{timestamp}.png"
+        )
 
-    depth_image.save(os.path.join(args.outdir, depth_output))
+        depth_image.save(os.path.join(args.outdir, depth_output))
 
     # Use the depth map to drive the image generation
     prompt = args.prompt
@@ -82,16 +95,20 @@ if __name__ == "__main__":
         # Convert grayscale to RGB
         depth_image = depth_image.convert("RGB")
 
+    # If seed is not provided, use a random seed
+    seed = args.seed if not None else int.from_bytes(os.urandom(2), "big")
+
+    generator = torch.Generator(device=device).manual_seed(seed)
     color_image = color_pipe(
         prompt,
         control_image=[depth_image],
         control_mode=[2],
-        controlnet_conditioning_scale=[0.5],
+        controlnet_conditioning_scale=[args.depth_weight],
         width=width,
         height=height,
-        num_inference_steps=6,
-        guidance_scale=3.5,
-        generator=torch.manual_seed(42),
+        num_inference_steps=args.steps,
+        guidance_scale=args.guidance,
+        generator=generator,
     ).images[0]
 
     color_output = (
