a1c5d882d7515f557c86abe3c5e147846a879520
Fabio Polimeni <fabio@ambient.run>
2024-09-30 11:46:31 +0200

    forgot to push the depth renderer files

---
+++ depth_renderer.py
@@ -0,0 +1,89 @@
+import os
+import torch
+from transformers import pipeline
+from PIL import Image
+from diffusers import FluxControlNetPipeline, FluxControlNetModel
+from diffusers.models import FluxMultiControlNetModel
+from dotenv import load_dotenv
+
+
+class DepthRenderer:
+    def __init__(self, base_model, controlnet_model):
+        self.base_model = base_model
+        self.controlnet_model = controlnet_model
+        self.device = self._get_device()
+        self.color_pipe = None
+        self.depth_pipe = None
+
+    def _get_device(self):
+        return (
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps" if torch.backends.mps.is_available() else "cpu"
+        )
+
+    def load_pipelines(self):
+        load_dotenv(".env")
+
+        hf_token = os.getenv("HF_TOKEN")
+        if not hf_token:
+            raise ValueError(
+                "HF_TOKEN not found in environment variables. Add it to your environment variables or to an .env file."
+            )
+
+        os.environ["HF_TOKEN"] = hf_token
+
+        print(f"Pipelines will run on {self.device}")
+
+        controlnet_union = FluxControlNetModel.from_pretrained(
+            self.controlnet_model, torch_dtype=torch.bfloat16
+        )
+        controlnet = FluxMultiControlNetModel([controlnet_union])
+        self.color_pipe = FluxControlNetPipeline.from_pretrained(
+            self.base_model, controlnet=controlnet, torch_dtype=torch.bfloat16
+        )
+        self.color_pipe.to(device=self.device)
+
+        self.depth_pipe = pipeline(
+            task="depth-estimation",
+            model="depth-anything/Depth-Anything-V2-Base-hf",
+            device=self.device,
+        )
+
+    def render_image(
+        self,
+        image,
+        prompt,
+        steps=4,
+        guidance=3.5,
+        is_depth=False,
+        depth_weight=0.5,
+        seed=None,
+    ):
+
+        depth_image = Image.open(image)
+
+        if not is_depth:
+            print("Converting image to depth")
+            depth_image = self.depth_pipe(depth_image)["depth"]
+
+        width, height = depth_image.size
+        depth_image = depth_image.convert("RGB")
+
+        seed = seed if seed is not None else int.from_bytes(os.urandom(2), "big")
+        generator = torch.manual_seed(seed)
+
+        print(f"Rendering image with seed {seed}")
+        color_image = self.color_pipe(
+            prompt,
+            control_image=[depth_image],
+            control_mode=[2],
+            controlnet_conditioning_scale=[depth_weight],
+            width=width,
+            height=height,
+            num_inference_steps=steps,
+            guidance_scale=guidance,
+            generator=generator,
+        ).images[0]
+
+        return color_image, depth_image
---
+++ depth_renderer_main.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+import time
+from depth_renderer import DepthRenderer
+from utils import ranged_type
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Image to render")
+
+    parser.add_argument("--image", type=str, required=True)
+    parser.add_argument("--prompt", type=str, required=True)
+    parser.add_argument("--steps", type=ranged_type(int, 1, 40), default=4)
+    parser.add_argument("--guidance", type=ranged_type(float, 0, 10), default=3.5)
+    parser.add_argument(
+        "--is_depth",
+        type=bool,
+        required=False,
+        default=False,
+        help="If true, the depth map will be extracred from the image",
+    )
+    parser.add_argument("--depth_weight", type=ranged_type(float, 0, 1), default=0.5)
+    parser.add_argument("--outdir", type=str, default="./outputs")
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="Seed for the generator. If None, a random seed will be used",
+    )
+    parser.add_argument(
+        "--save_depth",
+        type=bool,
+        default=False,
+        help="If true, the depth map will be saved",
+    )
+
+    args = parser.parse_args()
+
+    os.makedirs(args.outdir, exist_ok=True)
+
+    base_model = "black-forest-labs/FLUX.1-dev"
+    controlnet_model = "Shakker-Labs/FLUX.1-dev-ControlNet-Depth"
+    renderer = DepthRenderer(base_model, controlnet_model)
+    renderer.load_pipelines()
+
+    timestamp = int(time.time())
+    color_image, depth_image = renderer.render_image(
+        args.image,
+        args.prompt,
+        args.steps,
+        args.guidance,
+        args.is_depth,
+        args.depth_weight,
+        args.seed,
+    )
+
+    # Save depth image
+    if args.save_depth:
+        depth_output = (
+            f"{os.path.splitext(os.path.basename(args.image))[0]}_depth-{timestamp}.png"
+        )
+        depth_image.save(os.path.join(args.outdir, depth_output))
+
+    # Save color image
+    color_output = (
+        f"{os.path.splitext(os.path.basename(args.image))[0]}_color-{timestamp}.png"
+    )
+    color_image.save(os.path.join(args.outdir, color_output))
